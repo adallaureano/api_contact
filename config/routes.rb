d40ad4391414a0ec9f5788f8f52cad68469cf9e9
adallauriano <laureano.adalberto@gmail.com>
2020-04-12 13:45:18 +0100

    add api_contact

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      resources :contacts
+    end
+  end
+
+  #em caso de usar um subdominio
+  # constraints subdomain: 'api' do
+  #   scope module: 'api' do
+  #     namespace :v1 do
+  #       resources :contacts
+  #     end
+  #   end
+  # end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+
+
